8d3d4b21b729e924751b780e0c3f456df7be3409
dgoenka <divyansh.goenka@gmail.com>
2026-03-22 17:30:47 +0530

    sending json

--- routes/users.js
+++ routes/users.js
@@ -7,6 +7,16 @@ router.get('/', function(req, res, next) {
   res.json(users);
 });
 
+router.get('/:id', function(req, res, next) {
+ let user = users.find(user=> user.id == req.params.id);
+ if(user) res.json(user);
+ else {
+     let err = new Error("Not Found");
+     err.status = 404;
+     throw err;
+ }
+});
+
 /* GET users listing. */
 router.get('/alwaysthrows', function(req, res) {
     throw new Error("This always throws");
